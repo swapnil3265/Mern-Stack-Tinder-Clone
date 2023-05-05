c18ec7d71ca82a88dd241a0a2bb623ed29e08b5f
swapnil <swapnil3265@gmail.com>
2023-05-05 17:20:55 +0530

    tinder ver 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build the MERN TINDER CLONE</h1>
+      <Header />
+      {/* TinderCardsr */}
+      {/* SwipeButtons */}
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+        <h2>I am Header</h2>
+    </div>
+    
+  )
+}
+
+export default Header
